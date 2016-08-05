5106ddbf6cabe06b5a22136b9c18528077410988
Charisma Lieu <only@32mb.cn>
2016-08-05 15:43:08 +0800

    Add iptables-rules.sh

---
+++ iptables-rules.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+iptables -P INPUT DROP
+echo All Done
+exit
